b364a8e4d91a55db248331a18aa80ad5d52aeb7c
JorgeMariles <62728844+JorgeMariles@users.noreply.github.com>
2020-06-18 18:49:13 -0500

    Primer  avance
    Primer  avance

---
+++ Punto.d.v/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Punto.d.v
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.button2 = new System.Windows.Forms.Button();
+            this.cantidades = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(336, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Vender";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.textBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.textBox1.Location = new System.Drawing.Point(219, 88);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 27);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(693, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(473, 528);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Nombre";
+            this.columnHeader1.Width = 300;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Precio";
+            this.columnHeader2.Width = 80;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Unidades";
+            this.columnHeader3.Width = 88;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(403, 88);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Agregar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // cantidades
+            // 
+            this.cantidades.Location = new System.Drawing.Point(121, 88);
+            this.cantidades.Name = "cantidades";
+            this.cantidades.Size = new System.Drawing.Size(81, 27);
+            this.cantidades.TabIndex = 4;
+            this.cantidades.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Cantidades U o Kg";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(270, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Producto";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1222, 820);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cantidades);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.TextBox cantidades;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Punto.d.v/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LumenWorks.Framework.IO.Csv;
+using System.IO;
+using System.Diagnostics;
+
+namespace Punto.d.v
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+            autocomplete();
+            this.ActiveControl = textBox1;
+            textBox1.Focus();
+            cantidades.Text = "1";
+            
+
+        }
+        //no se hacerlo de otra manera 
+        static class Inventarioglob
+        {
+            public static List<SearchParameters> Inventario = getitems();
+        }
+
+        //lista global
+        public static List<SearchParameters> getitems()
+        {
+            
+            var csvTable = new DataTable();
+            using (var csvReader = new CsvReader(new StreamReader(System.IO.File.OpenRead(@"C:\Users\monit\Documents\Inventario\Inventario.csv")), true))
+            {
+                csvTable.Load(csvReader);
+            }
+            List<SearchParameters> Inventario = new List<SearchParameters>();
+            for (int i = 0; i < csvTable.Rows.Count; i++)
+            {
+                Inventario.Add(new SearchParameters { Nombre = csvTable.Rows[i][0].ToString(), Unidad_Kg = csvTable.Rows[i][1].ToString(), Precio = csvTable.Rows[i][2].ToString(), Inventario = csvTable.Rows[i][2].ToString() });
+               
+            }
+            return Inventario;
+        }
+        //lista type
+        public class SearchParameters
+        {
+            public string Nombre{ get; set; }
+            public string Unidad_Kg { get; set; }
+            public string Precio { get; set; }
+            public string Inventario { get; set; }
+        }
+
+
+        //auto completar
+        public void autocomplete()
+        {
+            AutoCompleteStringCollection data = new AutoCompleteStringCollection();
+            for(int i=0; i< Inventarioglob.Inventario.Count; i++) {
+                data.Add(Inventarioglob.Inventario[i].Nombre);
+            }
+            textBox1.AutoCompleteCustomSource = data;
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("test");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+      
+            ListViewItem item = new ListViewItem(textBox1.Text);
+            for (int i = 0; i < Inventarioglob.Inventario.Count; i++) {
+                if (textBox1.Text == Inventarioglob.Inventario[i].Nombre)
+                {
+                    string Price = Inventarioglob.Inventario[i].Precio;
+                    item.SubItems.Add(Price);
+                    break;
+                }
+            }
+
+            item.SubItems.Add(cantidades.Text);
+
+            listView1.Items.Add(item);
+
+            
+            textBox1.Clear();
+            this.ActiveControl = textBox1;
+            textBox1.Focus();
+            cantidades.Text = "1";
+
+           
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            foreach (object item in listView1.Items)
+            {
+                MessageBox.Show(listView1.Text);
+            }
+        }
+    }
+    
+}
